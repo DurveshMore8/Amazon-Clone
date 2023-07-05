0af8778d7f9c242955adeae7584faffee6243bdf
Durvesh <durveshmore2003@gmail.com>
2023-07-05 13:24:40 +0530

    User Sign In Completed along with State Management

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -44,6 +44,14 @@ class _AuthScreenState extends State<AuthScreen> {
     );
   }
 
+  void signInUser() {
+    authService.signIn(
+      context: context,
+      email: _emailController.text,
+      password: _passwordController.text,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -149,7 +157,7 @@ class _AuthScreenState extends State<AuthScreen> {
                   padding: const EdgeInsets.all(8),
                   color: GlobalVariables.backgroundColor,
                   child: Form(
-                    key: _signUpFormKey,
+                    key: _signInFormKey,
                     child: Column(
                       children: [
                         CustomTextfield(
@@ -168,7 +176,11 @@ class _AuthScreenState extends State<AuthScreen> {
                         ),
                         CustomButton(
                           text: 'Sign In',
-                          onTap: () {},
+                          onTap: () {
+                            if (_signInFormKey.currentState!.validate()) {
+                              signInUser();
+                            }
+                          },
                         ),
                       ],
                     ),
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const User = require("../models/user");
 const bcryptjs = require('bcryptjs');
-
 const authRouter = express.Router();
+const jwt = require('jsonwebtoken');
 
 authRouter.post("/api/signup", async (req, res) => {
   try {
@@ -28,5 +28,33 @@ authRouter.post("/api/signup", async (req, res) => {
     return res.status(500).json({error: e.message});
   }
 });
+
+authRouter.post('/api/signin', async (req, res) => {
+  try {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email});
+
+    if(!user) {
+      return res
+        .status(400)
+        .json({msg: 'User with this email does not exist!'});
+    }
+
+    const isMatch = await bcryptjs.compare(password, user.password);
+
+    if(!isMatch) {
+      return res
+        .status(400)
+        .json({msg: 'Incorrect Password.'});
+    }
+
+    const token = jwt.sign({id: user._id}, "passwordKey");
+
+    res.json({token, ...user._doc});
+  } catch(e) {
+    res.status(500).json({error: e.message});
+  }
+})
 
 module.exports = authRouter;
