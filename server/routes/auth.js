08ab600c8896ab7da108c6c11069b00edb2e3f58
Durvesh <durveshmore2003@gmail.com>
2023-07-05 18:49:40 +0530

    Problem for State Management solved

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -24,60 +24,56 @@ authRouter.post("/api/signup", async (req, res) => {
       name,
     });
     user = await user.save();
-    return res.json({user});
-  } catch(e) {
-    return res.status(500).json({error: e.message});
+    return res.json({ user });
+  } catch (e) {
+    return res.status(500).json({ error: e.message });
   }
 });
 
 authRouter.post("/api/signin", async (req, res) => {
   try {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
 
-    if(!user) {
+    if (!user) {
       return res
         .status(400)
-        .json({msg: "User with this email does not exist!"});
+        .json({ msg: "User with this email does not exist!" });
     }
 
     const isMatch = await bcryptjs.compare(password, user.password);
 
-    if(!isMatch) {
-      return res
-        .status(400)
-        .json({msg: "Incorrect Password."});
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Incorrect Password." });
     }
 
-    const token = jwt.sign({id: user._id}, "passwordKey");
+    const token = jwt.sign({ id: user._id }, "passwordKey");
 
-    res.json({token, ...user._doc});
-  } catch(e) {
-    res.status(500).json({error: e.message});
+    res.json({ token, ...user._doc });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 });
 
 authRouter.post("/tokenIsValid", async (req, res) => {
   try {
-    const token = req.header('x-auth-token');
-    if(!token) return res.json(false);
+    const token = req.header("x-auth-token");
+    if (!token) return res.json(false);
+    const verified = jwt.verify(token, "passwordKey");
+    if (!verified) return res.json(false);
 
-    const isVerified = jwt.verify(token, "passwordKey");
-    if(!isVerified) return res.json(false);
-
-    const user = await User.findById(isVerified.id)
-    if(!user) return res.json(false);
+    const user = await User.findById(verified.id);
+    if (!user) return res.json(false);
     res.json(true);
-  } catch(e) {
-    res.status(500).json({error: e.message});
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 });
 
-//GET USER DATA
-authRouter.get('/', auth, async (req, res) => {
+authRouter.get("/", auth, async (req, res) => {
   const user = await User.findById(req.user);
-  res.json({...user._doc, token: req.token});
+  res.json({ ...user._doc, token: req.token });
 });
 
 module.exports = authRouter;
