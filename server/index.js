7663c39cbb4398ebfebec9b0224a67c69fe677dc
Durvesh <durveshmore2003@gmail.com>
2023-07-05 09:17:28 +0530

    Changes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const authRouter = require('./routes/auth');
 
 const PORT = 3000;
 const app = express();
-const DB = "mongodb+srv://DurveshMore:durveshmore8@cluster0.4aeabqa.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(express.json());
 app.use(authRouter);
