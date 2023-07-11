89254640581a699b77f0a91bbc8d0ff340325a5e
Durvesh <durveshmore2003@gmail.com>
2023-07-11 12:54:35 +0530

    Interface Created for Address Screen

--- lib/features/cart/screens/cart_screen.dart
+++ lib/features/cart/screens/cart_screen.dart
@@ -25,16 +25,21 @@ class _CartScreenState extends State<CartScreen> {
     );
   }
 
-  void navigateToAddress() {
+  void navigateToAddress(int sum) {
     Navigator.pushNamed(
       context,
       AddressScreen.routeName,
+      arguments: sum.toString(),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user;
+    int sum = 0;
+    user.cart
+        .map((e) => sum += e['quantity'] * e['product']['price'] as int)
+        .toList();
 
     return Scaffold(
       appBar: PreferredSize(
@@ -128,7 +133,7 @@ class _CartScreenState extends State<CartScreen> {
               padding: const EdgeInsets.all(8),
               child: CustomButton(
                 text: 'Proceed to Buy (${user.cart.length})',
-                onTap: navigateToAddress,
+                onTap: () => navigateToAddress(sum),
                 color: Colors.yellow[600],
               ),
             ),
