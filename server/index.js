5b569442af7ea1922c8410dbce2c2467a2f1a698
Durvesh <durveshmore2003@gmail.com>
2023-07-13 18:22:19 +0530

    Server Updated

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const adminRouter = require("./routes/admin");
 const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
 
-MONGO_URI =
+const MONGO_URI =
   "mongodb+srv://DurveshMore:durveshmore8@cluster0.4aeabqa.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 3000;
 const app = express();
